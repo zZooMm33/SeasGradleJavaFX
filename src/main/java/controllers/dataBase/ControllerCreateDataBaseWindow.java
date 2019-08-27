b087d2a60a2947efe298318608ec02a103353595
zZooMm <mmmoonnn@mail.ru>
2019-08-27 14:46:10 +0300

    + создание БД

---
+++ src/main/java/controllers/dataBase/ControllerCreateDataBaseWindow.java
@@ -0,0 +1,100 @@
+package controllers.dataBase;
+
+        import javafx.collections.FXCollections;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.*;
+        import javafx.stage.Stage;
+        import storage.CreateDataBase;
+        import utils.PropReader;
+        import utils.StaticFields;
+
+        import java.io.File;
+
+public class ControllerCreateDataBaseWindow {
+
+    @FXML // fx:id="comboBoxStorageType"
+    private ComboBox comboBoxStorageType; // Value injected by FXMLLoader
+
+    @FXML // fx:id="checkBoxCreateTables"
+    private CheckBox checkBoxCreateTables; // Value injected by FXMLLoader
+
+    @FXML // fx:id="checkBoxSaveSettings"
+    private CheckBox checkBoxSaveSettings; // Value injected by FXMLLoader
+
+    @FXML // fx:id="textFieldDataBaseName"
+    private TextField textFieldDataBaseName; // Value injected by FXMLLoader
+
+    @FXML // fx:id="buttonCreate"
+    private Button buttonCreate; // Value injected by FXMLLoader
+
+    @FXML // fx:id="buttonCancel"
+    private Button buttonCancel; // Value injected by FXMLLoader
+
+    @FXML
+    void buttonCancelOnClick(ActionEvent event) {
+        Stage stage = (Stage) buttonCancel.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    void buttonCreateOnClick(ActionEvent event) {
+
+        if (textFieldDataBaseName.getText().length() == 0){
+            new Alert(Alert.AlertType.ERROR, "Введите имя новой базы данных.").show();
+        }
+        else if (textFieldDataBaseName.getText().chars()
+                .mapToObj(Character.UnicodeBlock::of)
+                .anyMatch(b -> b.equals(Character.UnicodeBlock.CYRILLIC))){
+            new Alert(Alert.AlertType.ERROR, "Имя базы данных не должно содержать Кириллицу.").show();
+        }
+        else if (new File(System.getProperty("user.home") + "/" + StaticFields.getNameProgram() + "/db/" + textFieldDataBaseName.getText()).exists()){
+            new Alert(Alert.AlertType.ERROR, "База данных с таким именем уже существует.").show();
+        }
+        else{
+            if (CreateDataBase.Create(textFieldDataBaseName.getText(),
+                    (String) comboBoxStorageType.getValue(),
+                    checkBoxCreateTables.isSelected(),
+                    checkBoxSaveSettings.isSelected()))
+            {
+                Stage stage = (Stage) buttonCancel.getScene().getWindow();
+                stage.close();
+
+
+                String host = null;
+                if (((String) comboBoxStorageType.getValue()).equals("databaseSQLite")){
+                    host = "jdbc:sqlite:" + System.getProperty("user.home") + "/" + StaticFields.getNameProgram() + "/db/" + textFieldDataBaseName.getText() + "/" + textFieldDataBaseName.getText() + ".db";
+                }
+                else
+                {
+                    host = "jdbc:h2:" + System.getProperty("user.home") + "/" + StaticFields.getNameProgram() + "/db/" + textFieldDataBaseName.getText() + "/" + textFieldDataBaseName.getText();
+                }
+
+
+                new Alert(Alert.AlertType.INFORMATION,
+                        "База данных была успешно создана.\n" +
+                                "host: " + host + "\n" +
+                                "user: sa\n" +
+                                "pass: null")
+                        .show();
+            }
+            else
+            {
+                new Alert(Alert.AlertType.ERROR, "Ошибка при создании базы данных.").show();
+            }
+        }
+
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        comboBoxStorageType.setValue("databaseSQLite");
+        comboBoxStorageType.setItems(FXCollections.observableArrayList("databaseSQLite", "databaseH2"));
+
+        // для проверки isSelected
+        checkBoxCreateTables.setSelected(true);
+        checkBoxSaveSettings.setSelected(true);
+    }
+
+}
--- src/main/java/storage/CreateDataBase.java
+++ src/main/java/storage/CreateDataBase.java
@@ -1,10 +1,83 @@
 package storage;
 
+import utils.PropReader;
+import utils.StaticFields;
+
+import java.io.*;
+
 public class CreateDataBase {
-    public static boolean Create(String name, String storageType, boolean createTable, boolean saveSettings){
+    public static boolean Create(String name, String storageType, boolean createTable, boolean saveSettings) {
+
+        new File(System.getProperty("user.home") + "/" + StaticFields.getNameProgram() + "/db").mkdir();
+        File propFilePath = new File(System.getProperty("user.home") + "/" + StaticFields.getNameProgram() + "/db/" + name);
+
+        if (storageType.equals("databaseSQLite")) {
+
+            try {
+
+                File dbFile = new File(propFilePath + "/" + name + ".db");
+                InputStream inputStream = CreateDataBase.class.getClassLoader().getResourceAsStream("emptyDataBase/SQLite/empty.db"); ///emptyDataBase/SQLite/
+
+                propFilePath.mkdir();
+                dbFile.createNewFile();
+
+                OutputStream outputStream = new FileOutputStream(dbFile);
+
+                int data;
+                while ((data = inputStream.read()) != -1) {
+                    outputStream.write(data);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
 
 
+        } else {
 
+            try {
+                File dbFile_1 = new File(propFilePath + "/" + name + ".h2.db");
+                File dbFile_2 = new File(propFilePath + "/" + name + ".trace.db");
+                InputStream inputStream_1 = PropReader.class.getClassLoader().getResourceAsStream("emptyDataBase/H2/empty.h2.db");
+                InputStream inputStream_2 = PropReader.class.getClassLoader().getResourceAsStream("emptyDataBase/H2/empty.trace.db");
+
+                propFilePath.mkdir();
+                dbFile_1.createNewFile();
+                dbFile_2.createNewFile();
+
+                OutputStream outputStream = new FileOutputStream(dbFile_1);
+                int data;
+                while ((data = inputStream_1.read()) != -1) {
+                    outputStream.write(data);
+                }
+
+                outputStream = new FileOutputStream(dbFile_2);
+                while ((data = inputStream_2.read()) != -1) {
+                    outputStream.write(data);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+
+        }
+
+        if (createTable){
+            // вызываем метод создания таблицы
+        }
+
+        if(saveSettings){
+            PropReader.setVal("storageType", storageType);
+            PropReader.setVal("user", "sa");
+            PropReader.setVal("pass", "null");
+
+            if (storageType.equals("databaseSQLite"))
+                PropReader.setVal("host", "jdbc:sqlite:" + System.getProperty("user.home") + "/" + StaticFields.getNameProgram() + "/db/" + name+ "/" + name + ".db");
+            else
+                PropReader.setVal("host", "jdbc:h2:" + System.getProperty("user.home") + "/" + StaticFields.getNameProgram() + "/db/" + name+ "/" + name);
+        }
 
         return true;
     }
